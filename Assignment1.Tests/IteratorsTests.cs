b2a75133d9fe598a90d96ac0936c763855bba573
Nicol <nihp>
2022-09-09 13:55:46 +0200

    Added filter and test for filter

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,21 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Filter_using_modulo_as_predicate_returns_list_of_2_and_4()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5};
+        public static bool isEven(int numb){
+            if (numb % 2 = 0){
+                return true;
+            }
+            else retun false;
+        }
+
+        Iterators.Filter(numbers, isEven);
+
+        Assert.Equal({2, 4}, Iterators.Filter(numbers, isEven););
+    }
+
+
 }
