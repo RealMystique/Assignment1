c79d7edaa4f4dd154888706400f09c7e4fbeec57
mohammadammar <mreh@itu.dk>
2022-09-09 13:57:18 +0200

    flatten

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,16 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Flatten_given_3_lists_returns_1_list()
+    {
+        // Arrange
+        int[] list = { int[] {1,2,3}, int[] {4,5,6}, int[] {7,8,9} };
+
+        // Act
+        var flatten = Iterators.Flatten(list)
+
+        // Assert
+        flatten.Should().BeEquivalentTo(new[] {1,2,3,4,5,6,7,8,9});
+    }
 }
