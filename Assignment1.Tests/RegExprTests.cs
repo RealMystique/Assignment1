8c6eb9e886b28e6671bf1e8625932d034bfb9c98
LocatedInSpace <48300341+LocatedInSpace@users.noreply.github.com>
2022-09-14 00:59:06 +0200

    regex Urls function added, aswell as test cases

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -85,4 +85,40 @@ public class RegExprTests
             "The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to." }
         );
     }
+    
+    [Fact]
+    public void Urls_extract()
+    {
+        string html = @"<div>
+    <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=""https://en.wikipedia.org/wiki/Theoretical_computer_science"" title=""Theoretical computer science"">theoretical computer science</a> and <a href=""https://en.wikipedia.org/wiki/Formal_language"" title=""Formal language"">formal language</a> theory, a sequence of <a href=""https://en.wikipedia.org/wiki/Character_(computing)"" title=""Character (computing)"">characters</a> that define a <i>search <a href=""https://en.wikipedia.org/wiki/Pattern_matching"" title=""Pattern matching"">pattern</a></i>. Usually this pattern is then used by <a href=""https://en.wikipedia.org/wiki/String_searching_algorithm"" title=""String searching algorithm"">string searching algorithms</a> for ""find"" or ""find and replace"" operations on <a href=""https://en.wikipedia.org/wiki/String_(computer_science)"" title=""String (computer science)"">strings</a>.</p>
+</div>";
+        // Act
+        var text = RegExpr.Urls(html);
+
+        // Assert
+        text.Should().BeEquivalentTo(new[] { 
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), 
+            (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"), 
+            (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"), 
+            (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"), 
+            (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), 
+            (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")
+        });
+    }
+    
+    [Fact]
+    public void Urls_arbitrary_tag_name_and_innerText_fallback()
+    {
+        string html = @"<div>
+    <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <qq href=""https://en.wikipedia.org/wiki/Theoretical_computer_science"" title=""Theoretical computer science"">theoretical computer science</qq> and <a href=""https://en.wikipedia.org/wiki/Formal_language"">formal language theory</a></p>
+</div>";
+        // Act
+        var text = RegExpr.Urls(html);
+
+        // Assert
+        text.Should().BeEquivalentTo(new[] { 
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), 
+            (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "formal language theory"), 
+        });
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -41,5 +41,23 @@ public static class RegExpr
         }
     }
 
-    public static IEnumerable<(Uri url, string title)> Urls(string html) => throw new NotImplementedException();
+    public static IEnumerable<(Uri url, string title)> Urls(string html)
+    {
+        // this works for title="", but not for if there is no title at all
+        //string pattern = @"<([a-zA-Z0-9]+)[^>]*?href=""(?<url>.*?)""[^>]*?title=""(?<title>.*?)""[^>]*?>(?<innertext>.+?)<\/\1>";
+        // this pattern is a little more unintuitive, and could potentially be flawed due to [^t>], but that restriction is what ensures a title match, if there actually is one 
+        //    this does though also mean, that throwing in another field that starts with t (that isnt title) would mean the match fails.
+        string pattern = @"<([a-zA-Z0-9]+)[^>]*?href=""(?<url>.*?)""[^t>]*?(title=""(?<title>.*?)"")*[^t>]*?>(?<innertext>.+?)<\/\1>";
+        
+        // this is my attempt at making it recognize still even with no title=, but for some reason it doesn't work, logically it seems sound :/
+        //string pattern = @"<([a-zA-Z0-9]+)[^>]*?href=""(?<url>.*?)""[^t>]*?(title=""(?<title>.*?)"")*[^>]*?>(?<innertext>.+?)<\/\1>";
+        // the idea is that   title=""(?<title>.*?)""  ->  (title=""(?<title>.*?)"")*    meaning that title would match either 0 or n, but this does not work
+        foreach (Match match in Regex.Matches(html, pattern,
+                     RegexOptions.None,
+                     TimeSpan.FromSeconds(1)))
+        {
+            yield return (new Uri(match.Result("${url}")),
+                match.Result("${title}") != "" ? match.Result("${title}") : match.Result("${innertext}"));
+        }
+    }
 }
