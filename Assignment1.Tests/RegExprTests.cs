b69024cb0864380b71bb322d91afdbd8a6b0b0b4
LocatedInSpace <48300341+LocatedInSpace@users.noreply.github.com>
2022-09-13 11:58:54 +0200

    SplitLine & Resolution in RegExpr implemented

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,25 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void SplitLine_lines_become_words()
+    {
+        var lines = new List<string> { "dog cat mosquito", "chips soda"};
+        // Act
+        var words = RegExpr.SplitLine(lines);
+
+        // Assert
+        words.Should().BeEquivalentTo(new[] {"dog", "cat", "mosquito", "chips", "soda"});
+    }
+    
+    [Fact]
+    public void Resolution_try_parse()
+    {
+        string resolution = "1920x1080";
+        // Act
+        var parsed = RegExpr.Resolution(resolution);
+
+        // Assert
+        parsed.Should().BeEquivalentTo(new[] {(1920, 1080)});
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,32 @@
 namespace Assignment1;
+using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        string pattern = @"[a-zA-Z0-9]+";
+        foreach (string line in lines)
+        {
+            foreach (Match match in Regex.Matches(line, pattern,
+                         RegexOptions.None,
+                         TimeSpan.FromSeconds(1)))
+                yield return match.Value;
+        }
+    }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+    {
+        string pattern = @"((?<width>\d+)x(?<height>\d+))+";
+        foreach (Match match in Regex.Matches(resolutions, pattern,
+                     RegexOptions.None,
+                     TimeSpan.FromSeconds(1)))
+        {
+            yield return ( Int32.Parse(match.Result("${width}")), Int32.Parse(match.Result("${height}")) );
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html) => throw new NotImplementedException();
 }
